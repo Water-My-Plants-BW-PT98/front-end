354b3b6397b8587f49677062689e0d58759a5022
jeremy black <jrebla85@yahoo.com>
2021-03-27 15:27:36 -0500

    Added an AddPlant component to enable a user to add a plant to their list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Signup from './Components/Signup'
 import Login from './Components/Login'
+import AddPlant from './Components/AddPlant'
 
 function App() {
   return (
     <>
       <Signup />
       <Login />
+      <AddPlant />
     </>
   );
 }
