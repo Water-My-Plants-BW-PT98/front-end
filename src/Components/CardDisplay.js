77ffe6e9918d10077118007ab82b9637b9c09f8f
jeremy black <jrebla85@yahoo.com>
2021-03-27 15:54:50 -0500

    Started Building the CardDisplay component for users plants.

---
+++ src/Components/CardDisplay.js
@@ -0,0 +1,34 @@
+// import { useState } from 'react'
+// import axios from 'axios'
+// import styled from 'styled-components'
+
+// const StyledDiv = styled.div`
+//     display: flex;
+//     flex-direction: column;
+//     justify-content: center;
+//     align-items: center;
+// `
+
+// const CardDisplay = () => {
+
+//     return(
+//         <StyledDiv>
+//             <h2>My Plants</h2>
+//             <div>
+//                 {props.state.plants (item => {
+//                     if(item.helper_id === null /* && item.status === false*/){
+//                         return <Plant key={item.id}>
+//                             <h3>{item.nickname}</h3>
+//                             <p>{item.species}</p>
+//                             <p>{item.h20Frequency}</p>
+//                             <button>Add Plant</button>
+//                             <button>Remove Plant</button>
+//                         </Plant>
+//                     }
+//                 })}
+//             </div>
+//         </StyledDiv>
+//     )
+// }
+
+// export default CardDisplay
