c56bdaa27816a3181bd5cfdb7c7cda577260b93d
mikecrbenton <mikecrbenton@hotmail.com>
2021-03-30 18:39:24 -0500

    cleaned up warnings for Vercel deploy

--- src/Components/CardDisplayMOCK.js
+++ src/Components/CardDisplayMOCK.js
@@ -1,6 +1,5 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 import { useHistory } from 'react-router-dom'
-import axios from 'axios'
 import styled from 'styled-components'
 
 //REDUX
--- src/Components/EditPlant.js
+++ src/Components/EditPlant.js
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import axios from 'axios'
 import styled from 'styled-components';
 
 //REDUX
