11df990df52b18fbe5b1158cccca42220669c4c4
mikecrbenton <mikecrbenton@hotmail.com>
2021-03-31 19:59:13 -0500

    fixed error warning for vercel deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import Header from './Components/Header'
 import Signup from './Components/Signup'
