d8a2dab56ee530bef8ee8777ac2f7655b0c672fc
mikecrbenton <mikecrbenton@hotmail.com>
2021-04-01 19:52:12 -0500

    added private routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch } from "react-router-dom";
+import PrivateRoute from './Utils/PrivateRoute'
 
 import Header from './Components/Header'
 import Signup from './Components/Signup'
@@ -7,9 +8,6 @@ import Login from './Components/Login'
 import AddPlant from './Components/AddPlant'
 import EditPlant from './Components/EditPlant'
 import EditUser from './Components/EditUser'
-
-
-//FOR DEVELOPMENT PHASE ONLY 3/28/21  MB
 import CardDisplay from './Components/CardDisplay'
 
 function App() {
@@ -19,10 +17,10 @@ function App() {
        <Switch>
           <Route path="/login" component={Login} />
           <Route path="/signup" component={Signup} />
-          <Route path ="/addplant" component={AddPlant} />
-          <Route path ="/carddisplay" component={CardDisplay} />
-          <Route exact path="/EditPlant/:id" component={EditPlant} />
-          <Route exact path="/EditUser" component={EditUser} />
+          <PrivateRoute path ="/addplant" component={AddPlant} />
+          <PrivateRoute path ="/carddisplay" component={CardDisplay} />
+          <PrivateRoute exact path="/EditPlant/:id" component={EditPlant} />
+          <PrivateRoute exact path="/EditUser" component={EditUser} />
         </Switch>
     </>
   );
--- src/Utils/PrivateRoute.js
+++ src/Utils/PrivateRoute.js
@@ -1,1 +1,25 @@
-import { axiosWithAuth } from '../utils/AxiosWithAuth';
+import { Route, Redirect } from "react-router-dom";
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+
+   const token = window.localStorage.getItem("token");
+   console.log("TOKEN: ",token)
+
+  //RETURNS A <ROUTE> COMPONENT
+  return (
+    <Route {...rest}  // rest operator takes an object and destructures it, and rebuilds it
+      // render prop that returns a component
+      render={ (props) => {
+        if (token) {
+          console.log("token found - success")
+          return <Component {...props} />;
+        } else {
+          console.log("error - no token")
+          return <Redirect to="/login" />;
+        }
+      }}
+    />
+  );
+};
+
+export default PrivateRoute;
